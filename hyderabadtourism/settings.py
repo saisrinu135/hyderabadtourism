cfb79d6a22c3821329656dc1ef17f67145de65dc
saisrinu <saisrinugampa135@gmail.com>
2023-11-23 10:58:55 +0530

    encrypted

--- hyderabadtourism/settings.py
+++ hyderabadtourism/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS=os.path.join(BASE_DIR,'templates')
@@ -20,10 +22,10 @@ TEMPLATES_DIRS=os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3fchra)#8b@4!)9z#s1hplm2qf+8_wa@s1ws8bj^^%aih^8r$b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = []
 
